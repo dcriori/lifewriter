b2b40f4da2569912757b6dbfdfa722272d86c7a6
david <david@xiaokakeji.com>
2016-09-21 08:55:07 +0800

    change

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var Evernote = require('evernote').Evernote;
 var config = require('../config.json');
 var callbackUrl = "http://localhost:3000/oauth_callback";
-var wechat = require('wechat');
+// var wechat = require('wechat');
 var config = {
   token: 'dcriori',
   appid: 'wxce68d383858d6357',
@@ -14,26 +14,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
-	wechat.handler(req, res);
-
-	//监听文本信息
-	wechat.text(function (data) {
-	//console.log(data.ToUserName);
-	//console.log(data.FromUserName);
-	//console.log(data.CreateTime);
-	//console.log(data.MsgType);
-	//...
-	var msg = {
-	  FromUserName : data.ToUserName,
-	  ToUserName : data.FromUserName,
-	  //MsgType : "text",
-	  Content : "这是文本回复",
-	  //FuncFlag : 0
-	}
-	//回复信息
-	wechat.send(msg);
-	});
 
 	res.redirect('index');
 });
