1dd2ef18106fe5574c305ab0d5d58d6b1f3a21ce
david <david@xiaokakeji.com>
2016-09-21 09:12:37 +0800

    change

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,3 @@
-var Evernote = require('evernote').Evernote;
-var config = require('../config.json');
-var callbackUrl = "http://localhost:3000/oauth_callback";
-// var wechat = require('wechat');
-var config = {
-  token: 'dcriori',
-  appid: 'wxce68d383858d6357',
-  encodingAESKey: 'G0YA9cPB6bxZrlYdpy9Ol8F7crRQTo1avymZn2Lsz1m'
-};
-
-
 var express = require('express');
 var router = express.Router();
 
